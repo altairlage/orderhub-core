82668ffd3f4d195e4188e0996a2f5f7b0791e582
Gaddini <ferreiragaddini@bol.com.br>
2025-07-23 00:02:01 -0300

    Fazendo a classe CriarPedidoDTO implementar o Serializable para poder ser postado na fila do RabbitMQ

--- orderhub-core/src/main/java/br/com/orderhub/core/dto/pedidos/CriarPedidoDTO.java
+++ orderhub-core/src/main/java/br/com/orderhub/core/dto/pedidos/CriarPedidoDTO.java
@@ -2,8 +2,9 @@ package br.com.orderhub.core.dto.pedidos;
 
 import br.com.orderhub.core.domain.enums.StatusPedido;
 
+import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 
-public record CriarPedidoDTO(Long idCliente, List<Map<String, Object>> listaQtdProdutos, StatusPedido status) {
+public record CriarPedidoDTO(Long idCliente, List<Map<String, Object>> listaQtdProdutos, StatusPedido status) implements Serializable {
 }
